fed14741b3b05c36143e41f7a4a2ab4572c884ac
summer-mbp <x.xiaokang@gmail.com>
2018-10-27 13:45:55 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
-const  koutoSwiss = require( "kouto-swiss" );
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+// const  koutoSwiss = require( "kouto-swiss" );
 
+// const stylesFile = new ExtractTextPlugin(path.join('/dist/styls.css'))
 
 module.exports = {
   mode: 'development',
@@ -11,21 +13,28 @@ module.exports = {
   },
   module: {
     rules: [
-    {
-      test: /\.js$/,
-      loader: 'babel-loader',
-      // include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
-    },
-    {
-      test: /\.styl$/,
-      loader: 'style-loader!css-loader!stylus-loader'
-    }]
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        // include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+      },
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'stylus-loader']
+        })
+      }]
   },
   resolve: {
     extensions: ['.json', '.js', '.jsx', '.css']
   },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ],
   devtool: 'source-map',
   devServer: {
+    host: '0.0.0.0',
     publicPath: path.join('/dist/')
   }
 };
